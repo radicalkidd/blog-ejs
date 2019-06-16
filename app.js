b4ce336f7f76cdb115233d993d16cdaa09b3394a
radicalkidd <vuqh92@gmail.com>
2019-06-16 10:31:42 -0700

    Route about and contact

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.get("/", function(req, res) {
   res.render("home", {homeContent: homeStartingContent});
 });
 
+app.get("/about", function(req, res) {
+  res.render("about", {aboutInfo: aboutContent});
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactInfo: contactContent});
+});
 
 
 
