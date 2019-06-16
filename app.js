15d09a4fa3bb58b5fef736c0f2649524ea6c7e45
radicalkidd <vuqh92@gmail.com>
2019-06-16 10:03:42 -0700

    Header and footer for home

--- app.js
+++ app.js
@@ -15,19 +15,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.render("home", {homeContent: homeStartingContent});
+});
 
 
 
 
 
 
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3003, function() {
+  console.log("Server started on port 3003");
 });
