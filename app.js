612cd64c52ffa30ce5c4a6267446de14c95fc1b0
radicalkidd <vuqh92@gmail.com>
2019-06-16 18:16:58 -0700

    Add form control on compose.ejs

--- app.js
+++ app.js
@@ -27,7 +27,14 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactInfo: contactContent});
 });
 
+app.get("/compose", function(req, res) {
+  res.render("compose")
+});
 
+app.post("/compose", function(req, res) {
+  console.log(req.body.titleInput);
+  console.log(req.body.inputPost);
+});
 
 
 
